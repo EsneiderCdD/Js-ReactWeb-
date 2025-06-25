ce3fccecf6e7f719067aa39b3f9e6559fde7a9c3
EsneiderCdD <ecd.97@hotmail.com>
2025-06-25 13:22:21 -0500

    Slider

--- src/pages/Slider/BasicSlider.jsx
+++ src/pages/Slider/BasicSlider.jsx
@@ -1,9 +1,38 @@
+import { useState } from 'react';
+import styles from './BasicSlider.module.css';
+
 export default function BasicSlider() {
+    const images = [
+        'https://i.pinimg.com/736x/0b/b9/8c/0bb98c92210c237df5b19efcba1dd21b.jpg',
+        'https://i.pinimg.com/736x/b2/25/b6/b225b631eea5c3196e7ef38ad28d5489.jpg',
+        'https://i.pinimg.com/736x/c7/96/e1/c796e171322df24e711fffdcfc4eed14.jpg'
+    ];
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const goToNext = () => {
+        if (currentIndex < images.length - 1) {
+            setCurrentIndex(currentIndex + 1);
+        } else {
+            setCurrentIndex(0); // Reinicia al inicio
+        }
+    };
+
+    const goToPrevious = () => {
+        if (currentIndex > 0) {
+            setCurrentIndex(currentIndex - 1);
+        } else {
+            setCurrentIndex(images.length - 1); // Va a la última imagen
+        }
+    };
+
     return (
-        <div>
-            <h3>Slider Básico</h3>
-            <p>Este es el contenido del slider básico.</p>
-            <input type="range" min="0" max="100" />
+        <div className={styles.slider}>
+            <img src={images[currentIndex]} alt={`Imagen ${currentIndex + 1}`} />
+            <div className={styles.controls}>
+                <button onClick={goToPrevious}>Anterior</button>
+                <button onClick={goToNext}>Siguiente</button>
+            </div>
         </div>
     );
 }
